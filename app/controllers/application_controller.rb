3e0c0eea928c129f2c60efc6958d5b18c0b0e09e
EC2 Default User <ec2-user@ip-172-31-21-26.ec2.internal>
2020-09-16 11:00:39 +0000

    [Finish]4-2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  #privateは自分のコントローラ内でしか参照できないから、
+  #呼び出された今回のコントローラではprotectedで参照する
+  protected
+
+  def configure_permitted_parameters
+    #nameのデータ操作を許可するアクションメソッドが指定されている
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
