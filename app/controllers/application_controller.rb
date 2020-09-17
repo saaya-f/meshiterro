9e45f3fbf36791a2ea2549c87aac1e23a0eedc70
EC2 Default User <ec2-user@ip-172-31-21-26.ec2.internal>
2020-09-17 09:24:39 +0000

    [Finish]4-3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   #privateは自分のコントローラ内でしか参照できないから、
   #呼び出された今回のコントローラではprotectedで参照する
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'f89356ce728b5b812d2f6c3c44eb6962ea49da2c504541ab8dac51b5a4b12ae400ca003938801d435c1ca206fe79e40753d46026e9151fc09a8b68c43d0d94a0'
